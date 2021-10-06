67adf797d6c5635a7e4c0251c31390da58c67ca1
Aleksa Stojanović <aleksa.stojanovic@ecd.rs>
2021-10-07 00:11:53 +0200

    current state of the strategy;

---
+++ config/constants.py
@@ -0,0 +1,5 @@
+import os
+
+mainnet = {
+    "YEARN_USDC_VAULT": os.environ['YEARN_USDC_VAULT']
+}
---
+++ tests/conftest.py
@@ -0,0 +1,93 @@
+import pytest
+from brownie import config
+from brownie import Contract
+
+@pytest.fixture
+def gov(accounts):
+    yield accounts.at("0xFEB4acf3df3cDEA7399794D0869ef76A6EfAff52", force=True)
+
+
+@pytest.fixture
+def user(accounts):
+    yield accounts[0]
+
+
+@pytest.fixture
+def rewards(accounts):
+    yield accounts[1]
+
+
+@pytest.fixture
+def guardian(accounts):
+    yield accounts[2]
+
+
+@pytest.fixture
+def management(accounts):
+    yield accounts[3]
+
+
+@pytest.fixture
+def strategist(accounts):
+    yield accounts[4]
+
+
+@pytest.fixture
+def keeper(accounts):
+    yield accounts[5]
+
+
+@pytest.fixture
+def token():
+    token_address = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"  # this should be the address of the ERC-20 used by the strategy/vault (DAI -> changed to USDC)
+    yield Contract(token_address)
+
+
+@pytest.fixture
+def amount(accounts, token, user):
+    amount = 10_000 * 10 ** token.decimals()
+    # In order to get some funds for the token you are about to use,
+    # it impersonate an exchange address to use it's funds.
+    reserve = accounts.at("0x39AA39c021dfbaE8faC545936693aC917d5E7563", force=True)
+    token.transfer(user, amount, {"from": reserve})
+    yield amount
+
+
+@pytest.fixture
+def weth():
+    token_address = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
+    yield Contract(token_address)
+
+
+@pytest.fixture
+def weth_amout(user, weth):
+    weth_amout = 10 ** weth.decimals()
+    user.transfer(weth, weth_amout)
+    yield weth_amout
+
+
+@pytest.fixture
+def vault(pm, gov, rewards, guardian, management, token):
+    Vault = pm(config["dependencies"][0]).Vault
+    vault = guardian.deploy(Vault)
+    vault.initialize(token, gov, rewards, "", "", guardian, management)
+    vault.setDepositLimit(2 ** 256 - 1, {"from": gov})
+    vault.setManagement(management, {"from": gov})
+    yield vault
+
+@pytest.fixture
+def external_vault():
+    usdc_vault_address = "0x5f18C75AbDAe578b483E5F43f12a39cF75b973a9"
+    yield Contract.from_explorer(usdc_vault_address)
+
+@pytest.fixture
+def strategy(strategist, keeper, vault, external_vault, Strategy, gov):
+    strategy = strategist.deploy(Strategy, external_vault, vault)
+    strategy.setKeeper(keeper)
+    vault.addStrategy(strategy, 10_000, 0, 2 ** 256 - 1, 1_000, {"from": gov})
+    yield strategy
+
+
+@pytest.fixture(scope="session")
+def RELATIVE_APPROX():
+    yield 1e-5
